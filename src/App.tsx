8e19ebe17cb00aa012e78a1dbf569356d2da3332
fegloff <fegloff@gmail.com>
2022-07-22 11:21:12 -0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
-// import Authentication from './routes/authentication/authentication.component';
 import Mint from './routes/mint/mint.component';
-// import Landing from './routes/landing/landing.component';
 import Gallery from './routes/gallery/gallery.component';
+// import Landing from './routes/landing/landing.component';
 // import Home from './routes/home/home.component';
-import './App.scss';
+// import Authentication from './routes/authentication/authentication.component';
+
 import Checkout from './routes/checkout/checkout.component';
 import CheckoutSuccess from './routes/checkout/checkout-success.component';
 import CheckoutFailure from './routes/checkout/checkout-failure.component';
+
+import './App.scss';
 
 // todo Protected/public routes
 const App = () => {
