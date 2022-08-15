847a094fd02a4800acfd08b25482a4b60c92e42b
Victa Kwok Wai Phu <victa.phu@gmail.com>
2022-08-16 02:51:20 +1000

    update: added login check in getting started
    case where user login from front end; however the backend session has expired

--- src/routes/get-started/get-started.component.tsx
+++ src/routes/get-started/get-started.component.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/header/header.component";
+import { checkingLogin, setLoginChecked } from "../../store/user/user.action";
+import { selectCheckLogin } from "../../store/user/user.selector";
+import { checkLogin } from "../../utils/mint-interface/mint-inteface.utils";
 
 const GetStarted = () => {
+  const [checked, setChecked] = useState(false)
+
+  const dispatch = useDispatch();
+  // getting started can verify that a user is logged in or not (same as navigation)
+  useEffect(() => {
+    if (checked) {
+      return;
+    }
+    dispatch(checkingLogin())
+    checkLogin().then(user=>dispatch(setLoginChecked(user))).catch(err=>dispatch(setLoginChecked(null)))
+    
+    setChecked(true)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [checked])
+
   return (
     <>
       <Header />
