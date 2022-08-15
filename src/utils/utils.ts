338fdc4cfacd8da7e45d046c4dcac96a9584cff2
fegloff <fegloff@gmail.com>
2022-08-15 16:42:46 -0500

    fix styling issues in create-collectible

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,3 +9,13 @@ export const checkHref = (url : string) : string => {
   }
   return '//' + url;
 }
+
+
+export const truncateAddressString = (address: string, num = 12) => {
+  if (!address) {
+    return '';
+  }
+  const first = address.slice(0, num);
+  const last = address.slice(-num);
+  return `${first}...${last}`;
+}
