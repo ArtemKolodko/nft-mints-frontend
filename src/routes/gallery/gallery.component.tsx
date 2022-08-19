9763b4ea413bcae3f1cda5ed72412fdb43d5e07b
artemkolodko <temakolodko@gmail.com>
2022-08-19 08:51:41 +0100

    Enable acces-pass route

--- src/routes/gallery/gallery.component.tsx
+++ src/routes/gallery/gallery.component.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import {useNavigate, useParams, useSearchParams} from "react-router-dom";
 import NftCard from "../../components/nft-card/nft-card.component";
 import {ApiTokenResponseType, CollectionType, TokenTypeEnum} from "../../types";
 import {
